5eb65e996b00080c6c6a18bb7dc989011ee7f8dd
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2020-12-08 03:32:34 +0600

    Changed email of admin account

--- src/main/resources/db/migration/V2020_12_01_00_05__add_admin_account_to_user_table.sql
+++ src/main/resources/db/migration/V2020_12_01_00_05__add_admin_account_to_user_table.sql
@@ -2,7 +2,7 @@
 
 insert into "user" values (
     '66397c00-87d0-4c61-ae9d-8d982c5be108',
-    'admin@admin.com',
+    'admin@deal.management',
     '$2a$10$CeHl4PCY4WlDhlEsGbqaFebcH69LJyTDJVqyLipj5ZOZyy.wLrgTm', -- it's a hash for "j32YLvc6Ls" password
     'ROLE_ADMIN'::role_type
 );
